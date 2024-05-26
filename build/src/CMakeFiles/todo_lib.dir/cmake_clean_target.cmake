e82344cdec68f1ed3b8e86f27d074a32c7d6344d
Aadith Jagan <aadithjonline@gmail.com>
2024-05-26 17:56:31 +0530

    Revert "Fixed" Error resolved This reverts commit e2c3e22fc04cb8a5e1443dceb70ba2773cab2f87.

--- build/src/CMakeFiles/todo_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtodo_lib.a"
-)
--- build/src/CMakeFiles/todo_lib.dir/helloworld.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/todo_lib.dir/helloworld.cpp.obj: \
- E:\---Projects---\TODO\ App\ -\ C++\todo\src\helloworld.cpp \
- E:/---Projects---/TODO\ App\ -\ C++/todo/include/todo_lib/helloworld.h
